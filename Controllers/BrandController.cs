ab1d2a07b2eda346c64955e49945e4dbc3129c55
jafare2010@gmail.com <jafare2010@gmail.com>
2022-01-03 19:15:44 +0300

    First Commit

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WesbServceDemo.Models;
+
+namespace WesbServceDemo.Controllers
+{
+    public class BrandController : ApiController
+    {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        public List<Brand> GetBrands()
+        {
+            var brands = _context.Brands.ToList();
+            return brands;
+        }
+
+        public Brand GetBrand(int id)
+        {
+            var brand = _context.Brands.Where(br => br.Id == id).FirstOrDefault();
+            return brand;
+        }
+        public void Post(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+        }
+        public void Put(Brand brand)
+        {
+            var exsitingBrand = _context.Brands.Where(br => br.Id == brand.Id).FirstOrDefault();
+            exsitingBrand.Name = brand.Name;
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+         var brand =_context.Brands.Where(br => br.Id == id).FirstOrDefault();
+            _context.Brands.Remove(brand);
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ Migrations/202201031229318_First Migration.cs
@@ -0,0 +1,26 @@
+﻿namespace WesbServceDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Brands",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Brands");
+        }
+    }
+}
